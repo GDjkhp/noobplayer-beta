27ba49a66e9b3589ef7dc558542620e0b2cec1ae
GDjkhp <jkhp.jkpa@gmail.com>
2026-09-15 23:20:55 +0800

    it doesnt make any difference

--- server.py
+++ server.py
@@ -229,7 +229,7 @@ PCM_CHANNELS = 2
 PCM_FRAME_SAMPLES = 960                                    # 20ms @ 48kHz
 PCM_FRAME_BYTES = PCM_FRAME_SAMPLES * PCM_CHANNELS * 2      # s16le
 OPUS_BITRATE = 96000
-LISTENER_QUEUE_MAX = 1024   # chunks; a slow client gets old ones dropped, not a backlog
+LISTENER_QUEUE_MAX = 8   # chunks; a slow client gets old ones dropped, not a backlog
 
 
 class _CallbackIO:
