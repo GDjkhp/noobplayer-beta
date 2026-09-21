9549d4809af0f20ba514d8f8e7bce6ae22766cd3
GDjkhp <jkhp.jkpa@gmail.com>
2026-09-21 22:21:34 +0800

    revert to stun only

--- server.py
+++ server.py
@@ -130,14 +130,14 @@ ice_servers = [
     RTCIceServer(
         urls=[
             "stun:stun.cloudflare.com:3478",
-            "turn:turn.cloudflare.com:3478?transport=udp",
-            "turn:turn.cloudflare.com:3478?transport=tcp",
-            "turns:turn.cloudflare.com:5349?transport=tcp",
-            "turn:turn.cloudflare.com:80?transport=tcp",
-            "turns:turn.cloudflare.com:443?transport=tcp"
+            # "turn:turn.cloudflare.com:3478?transport=udp",
+            # "turn:turn.cloudflare.com:3478?transport=tcp",
+            # "turns:turn.cloudflare.com:5349?transport=tcp",
+            # "turn:turn.cloudflare.com:80?transport=tcp",
+            # "turns:turn.cloudflare.com:443?transport=tcp"
         ],
-        username = os.getenv("TURN_USER"),
-        credential = os.getenv("TURN_PASS"),
+        # username = os.getenv("TURN_USER"),
+        # credential = os.getenv("TURN_PASS"),
     )
 ]
 
