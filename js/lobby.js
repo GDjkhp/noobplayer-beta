b3815ac53462dca47d50f6ba41b71f653862bda0
GDjkhp <jkhp.jkpa@gmail.com>
2026-09-21 22:54:25 +0800

    transport policy all

--- js/lobby.js
+++ js/lobby.js
@@ -273,8 +273,8 @@ const Lobby = {
               urls: 'stun:://google.com'
             }
           ],
-          // iceTransportPolicy: 'all', // Explicitly allow host, srflx (STUN) paths
-          // iceCandidatePoolSize: 4    // Pre-gather local interfaces early to avoid slow timeouts
+          iceTransportPolicy: 'all', // Explicitly allow host, srflx (STUN) paths
+          iceCandidatePoolSize: 4    // Pre-gather local interfaces early to avoid slow timeouts
         }
       );
       S.lobby.pc = pc;
